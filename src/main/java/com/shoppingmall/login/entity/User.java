a7f409bce09620f0efc0e605af60cdc563575f02
TeraSeo <seotj0413@gmail.com>
2024-05-29 01:41:52 +0900

    29th May, 2024

---
+++ src/main/java/com/shoppingmall/login/entity/User.java
@@ -0,0 +1,28 @@
+package com.shoppingmall.login.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+}
