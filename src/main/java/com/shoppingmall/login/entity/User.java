2fff3d5136c36751158b718e003812d0fe4bc596
TeraSeo <seotj0413@gmail.com>
2024-06-01 01:44:40 +0900

    config edited

--- src/main/java/com/shoppingmall/login/entity/User.java
+++ src/main/java/com/shoppingmall/login/entity/User.java
@@ -1,10 +1,7 @@
 package com.shoppingmall.login.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Table(name = "user")
